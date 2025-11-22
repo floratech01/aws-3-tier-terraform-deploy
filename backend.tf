458f4a6d56cb3f567a1bdcec7a7a2d55f97ae260
obianuju <obianujuflorence02@gmail.com>
2025-11-22 04:24:28 -0800

    gitrunner server ip authenticated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "digitalwitchngbucketcloud1"
-    key    = "digitalwitchng/prodution/terraform.tfstate"
-    region = "us-west-1"
+    bucket = "floratechcloud1.online"
+    key    = "floratech/production/terraform.tfstate"
+    region = "us-east-1"
   }
 }
